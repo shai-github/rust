0184c1eeebe3bb3c80a5caddc3de4d403717d3a3
shai-github <shaigithub@gmail.com>
2023-10-02 22:10:58 -0500

    upload package basics

---
+++ package/project/src/main.rs
@@ -0,0 +1,9 @@
+/*
+Rust follows the convention that, if you have main.rs defined in source directory,
+a binary crate with same name as package will be created automatically and main.rs will
+be the crate root.
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
